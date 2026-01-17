08aafed4a154fcaf46b6700fae9d35d4b58b966d
jamm-king <jmcylove@gmail.com>
2026-01-17 14:59:26 +0900

    fix: avoid tx around image polling

--- src/main/kotlin/com/jammking/loopbox/application/service/PollImageGenerationResultService.kt
+++ src/main/kotlin/com/jammking/loopbox/application/service/PollImageGenerationResultService.kt
@@ -10,11 +10,9 @@ import com.jammking.loopbox.domain.port.out.ImageGenerationTaskRepository
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
 
 @Service
-@Transactional
 class PollImageGenerationResultService(
     private val taskRepository: ImageGenerationTaskRepository,
     private val imageAiRouter: ImageAiRouter,
